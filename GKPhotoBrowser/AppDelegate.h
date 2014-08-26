2e8a22817500a9610694d3cce674d35791ba09f8
Gregory Klein <gregoryhklein@gmail.com>
2014-08-25 21:08:30 -0400

    Initial Commit

---
+++ GKPhotoBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GKPhotoBrowser
+//
+//  Created by Klein, Greg on 8/25/14.
+//  Copyright (c) 2014 HardFlip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GKPhotoBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GKPhotoBrowser
+//
+//  Created by Klein, Greg on 8/25/14.
+//  Copyright (c) 2014 HardFlip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
