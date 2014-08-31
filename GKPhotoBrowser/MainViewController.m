fc89477d68dddc27ff62ba2759b144523b364939
Gregory Klein <gregoryhklein@gmail.com>
2014-08-31 13:53:42 -0400

    Testing the browser more thoroughly

--- GKPhotoBrowser/MainViewController.m
+++ GKPhotoBrowser/MainViewController.m
@@ -13,11 +13,10 @@
 
 @property (weak) IBOutlet UIView* photoBrowserContainer;
 @property (weak) IBOutlet UIView* photoBrowserContainer2;
+@property (weak) IBOutlet UIView* photoBrowserContainer3;
 @property (nonatomic) GKPhotoBrowser* photoBrowser1;
 @property (nonatomic) GKPhotoBrowser* photoBrowser2;
-
-@property (weak) IBOutlet NSLayoutConstraint* heightConstraint;
-@property (weak) IBOutlet NSLayoutConstraint* widthConstraint;
+@property (nonatomic) GKPhotoBrowser* photoBrowser3;
 
 @property (nonatomic) BOOL enlarged;
 
@@ -34,19 +33,24 @@
    [self.photoBrowser1 addBrowserToContainerView:self.photoBrowserContainer];
    
    self.photoBrowser1.image = [UIImage imageNamed:@"Low in carrige"];
-   self.photoBrowser1.text = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
-   
-   [self addChildViewController:self.photoBrowser1];
-   [self.photoBrowser1 didMoveToParentViewController:self];
+//   self.photoBrowser1.text = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
+   self.photoBrowser1.text = @"Holes can form as a result of damage on the roll surface.  Abrasions, dings, & cuts can usually be removed by pulling film off the roll until the affected area has been removed.  The source of this type of damage is usually a result of transit damage, or rough handling.";
 
    self.photoBrowser2 = [GKPhotoBrowser browser];
    [self.photoBrowser2 addBrowserToContainerView:self.photoBrowserContainer2];
 
    self.photoBrowser2.image = [UIImage imageNamed:@"Spacer Ring"];
-   self.photoBrowser2.text = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
+//   self.photoBrowser2.text = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
+   self.photoBrowser2.text = @"Holes can form as a result of damage on the roll surface.  Abrasions, dings, & cuts can usually be removed by pulling film off the roll until the affected area has been removed.  The source of this type of damage is usually a result of transit damage, or rough handling.";
 
-   [self addChildViewController:self.photoBrowser2];
-   [self.photoBrowser2 didMoveToParentViewController:self];
+
+   self.photoBrowser3 = [GKPhotoBrowser browser];
+   [self.photoBrowser3 addBrowserToContainerView:self.photoBrowserContainer3];
+
+   self.photoBrowser3.image = [UIImage imageNamed:@"Core Extension"];
+   //   self.photoBrowser3.text = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
+   self.photoBrowser3.text = @"Holes can form as a result of damage on the roll surface.  Abrasions, dings, & cuts can usually be removed by pulling film off the roll until the affected area has been removed.  The source of this type of damage is usually a result of transit damage, or rough handling.";
+
 }
 
 @end
